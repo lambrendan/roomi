8bce08df15a456bb1e30b543d059b396db4b6bce
Brendan Lam <Brendan@Brendans-MacBook-Pro-2.local>
2018-11-06 15:40:59 -0800

    Updated database

---
+++ routes.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+const express = require('express');
+const router = express.Router();
+
+
+router.post('/signup', function(req,res ) {
+
+})
+
+router.post('/signin', function(req,res) {
+
+})
+
+router.post('/reset', function(req,res) {
+})
--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 let express = require('express');
-var app = express();
-var router = express.Router()
+let app = express();
+let mysql = require('mysql');
+let router = express.Router();
+let bodyParser = require('body-parser'); 
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
+var session = require('express-session');
 
+
+app.use(bodyParser.urlencoded( { extended: false }));  
+app.use(bodyParser.json());
 
 app.use('/', router);
+
+const connection = mysql.createConnection({
+    host:"localhost",
+    user: "root",
+    password: "roomicse110"
+});
+
+connection.connect(function(err) {
+    if( err ) {
+        throw err;
+    }
+    else {
+        console.log("Connected!");
+    }
+})
+
 router.get('/', function(req, res) {
     res.send( "hi");
 });
