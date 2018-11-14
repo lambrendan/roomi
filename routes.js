d2258bca70666ec5eb1ec7e18a06d0c1a326a7f5
Brendan Lam <Brendan@wireless-169-228-74-59.ucsd.edu>
2018-11-13 17:27:03 -0800

    Added the signup route

--- routes.js
+++ routes.js
@@ -2,9 +2,58 @@ const mysql = require('mysql');
 const express = require('express');
 const router = express.Router();
 
+const connection = mysql.createConnection({
+    host:"localhost",
+    user: "root",
+    password: "roomicse110",
+    database: "roomiDB"
+});
+
+connection.connect(function(err) {
+    if( err ) {
+        throw err;
+    }
+    else {
+        console.log("Connected!");
+        var sql = "CREATE TABLE IF NOT EXISTS users (firstName VARCHAR(255), lastName VARCHAR(255), email VARCHAR(255), password VARCHAR(255) )";
+        connection.query(sql, function(err, result) {
+            if( err ) {
+                throw err;
+            }
+            else {
+                console.log("Table Done");
+            }
+        })
+    }
+})
+
+router.get('/', function(req,res) {
+    console.log("hi");
+    res.send("hi");
+})
 
 router.post('/signup', function(req,res ) {
-
+    var user = {
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName,
+        "email": req.body.email,
+        "password": req.body.password,
+    }
+    connection.query('INSERT INTO users SET ?', user, function(error, results, fields) {
+        if( error ) {
+            res.send({
+                "code": 400,
+                "failed": true
+            })
+        }
+        else {
+            res.send({
+                "code": 200,
+                "message": "Successfully registered user",
+                "failed": false
+            })
+        }
+    })
 })
 
 router.post('/signin', function(req,res) {
@@ -13,3 +62,5 @@ router.post('/signin', function(req,res) {
 
 router.post('/reset', function(req,res) {
 })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,35 +1,15 @@
 let express = require('express');
 let app = express();
 let mysql = require('mysql');
-let router = express.Router();
+let router = require('./routes');
 let bodyParser = require('body-parser'); 
 var passport = require('passport');
 var localStrategy = require('passport-local').Strategy;
 var session = require('express-session');
 
-
 app.use(bodyParser.urlencoded( { extended: false }));  
 app.use(bodyParser.json());
 
 app.use('/', router);
-
-const connection = mysql.createConnection({
-    host:"localhost",
-    user: "root",
-    password: "roomicse110"
-});
-
-connection.connect(function(err) {
-    if( err ) {
-        throw err;
-    }
-    else {
-        console.log("Connected!");
-    }
-})
-
-router.get('/', function(req, res) {
-    res.send( "hi");
-});
 
 app.listen(3000, '0.0.0.0');
