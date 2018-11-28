e28e9fe4d712f108fc2814e44cc3a39ddee21121
ethsu1 <eth.fmt@gmail.com>
2018-11-27 19:28:46 -0800

    ui done

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -2,11 +2,15 @@ import Dashboard from "layouts/Dashboard/Dashboard.jsx";
 import Initial from "layouts/Initial/Initial.jsx";
 import Login from "layouts/Login/Login.jsx";
 import Signup from "layouts/Signup/Signup.jsx"
+import Chores from "../views/Chores/Chores";
+import ParkingSchedule from "../views/ParkingSchedule/ParkingSchedule.jsx";
 
 var indexRoutes = [
     { path: "/dashboard", name: "Home", component: Dashboard}, 
     { path: "/login", name: "Login", component: Login },
-    { path: "/signup", name: "Signup", component: Signup}
+    { path: "/signup", name: "Signup", component: Signup},
+    { path: "/chores", name: "Chores", component: Chores },
+    { path: "/parkingschedule", name: "Parking Schedule", component: ParkingSchedule }
 ];
 
 export default indexRoutes;
--- client/src/views/Chores/Chores.jsx
+++ client/src/views/Chores/Chores.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { RoomiTable } from "components/Table/RoomiTable.jsx";
+import { Button } from 'react-bootstrap'
 
 class Chores extends React.Component {
-
+    handleOnClick(){
+        console.log("add task");
+    }
     render() {
-        return null;
+        let task = [ "dishes", "vaccum", "trash"];
+        let assignee = ["Bob", "Rob", "Mike"]
+        let heading = ["Task", "Assignee"]
+        return(
+            <div>
+                <RoomiTable data={task} heading={heading} assignee={assignee} />
+                <input type="text"/>
+                <Button onClick={this.handleOnClick}>Add Task</Button>
+            </div>
+        )
     }
 }
 
