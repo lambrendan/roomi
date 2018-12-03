ed603c297d5216d587aaf512cdd1ad70ceba1e20
ethsu1 <eth.fmt@gmail.com>
2018-12-02 21:03:24 -0800

    working on remainders

--- client/src/views/Polls/Polls.jsx
+++ client/src/views/Polls/Polls.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import Sidebar from '../../components/Sidebar/Sidebar';
-
+import { Button } from 'react-bootstrap';
+import styles from './Polls.css';
 
 class Polls extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: '',
+            currentMsg: '',
+        }
+        this.handleOnClick = this.handleOnClick.bind(this);
+        this.componentDidMount = this.componentDidMount.bind(this);
+    }
+    handleOnClick(){
+
+    }
+    componentDidMount(){
+        this.setState({currentMsg: this.props.currentMessage});
+    }
 
     render() {
         return(
-            <Sidebar {...this.props} />
+            <form>
+                <label>Current Reminder: </label>
+                <label>{this.state.currentMsg}</label>
+                <Button className="delete" onClick={this}>Delete Current Reminder</Button>
+                <input type="input" name="name" style={styles}/>
+          </form>
         );
     }
 }
