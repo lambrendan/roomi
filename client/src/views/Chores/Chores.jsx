6ad6c3656ee3997ceca1220a95e42eee76817b9e
Brendan Lam <Brendan@Brendans-MBP-2.attlocal.net>
2018-12-09 03:49:53 -0800

    Took out shuffle-array

--- client/src/views/Chores/Chores.jsx
+++ client/src/views/Chores/Chores.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Table, Button } from "react-bootstrap";
 import axios from "axios";
-import shuffle from 'shuffle-array';
+
 
 class Chores extends React.Component {
     constructor(props){
